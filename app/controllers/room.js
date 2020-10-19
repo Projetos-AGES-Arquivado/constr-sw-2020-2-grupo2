bf30a6f5573648c26dbf70e692949894ab6b5258
Leonardo Veiga <leonardo.freitas.veiga@gmail.com>
2020-10-19 14:49:33 -0300

    Setting HTTP response codes

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -5,52 +5,59 @@ module.exports = function(app){
     var Room = app.models.room;
 
     var controller = {
+        
         index: function(req,res){
             Room.find({},[],{sort: {number:1} }).exec().then((rooms) => {
-                res.send(rooms);
+                if(rooms) {
+                    res.status(200).send(rooms)
+                } else {
+                    res.sendStatus(404)
+                }
             })
             
         },
 
-        newRoom:function(req,res){
-            const room = new Room(req.body);
-            room.save(function(err,room){
-                console.log(err);
-
-                if(err)
-                    res.status(500).end();
-                else
-                    res.json(room);
-            });
-        },
-
-        remove: function(req,res){
-            const id = req.params.id;
-
-            Room.deleteOne({_id: id}, function(err){
-            if(!err)
-                res.json({message: 'sucess'});
-            else
-                res.status(500).end();
-            })
-        },
-
-        room: async function(req,res){
+        findById: async function(req,res){
             const id = req.params.id;
             const response = await Room.findById(id).exec();
-            console.log(id)
-            console.log(response)
-            res.json(response)
+            if(response) {
+                res.status(200).send(response)
+            } else {
+                res.sendStatus(404)
+            }
         },
 
-        getByAtribute: function(req,res){
+        findByAtributes: function(req,res){
             const atribute = req.params.atribute;
-            const parse = req.query;
-            console.log(parse);
+            const parse = req.query
+            console.log(parse)
             res.sendStatus(200)
         },
 
-        updateRoomAll: function(req,res){
+        createRoom: function(req,res){
+            const room = new Room(req.body);
+            room.save(function(err,room){
+                if(err)
+                    res.status(500).end()
+                else
+                    res.status(201).send(room)
+            });
+        },
+
+        updateRoom: function(req,res){
+            const id = req.params.id;
+            const query = req.body
+            try{
+                const response = Room.findOneAndUpdate({_id:id},query).exec()
+            }catch{
+                res.sendStatus(404)
+            }
+            console.log(id)
+            console.log(response)
+            res.status(204).json("sucess")
+        },
+
+        replaceRoom: function(req,res){
             let { error } = schema.validate(req.body)
             if (error) return res.status(400).send(error.details)
             const id = req.params.id;
@@ -61,23 +68,19 @@ module.exports = function(app){
                 res.sendStatus(404)
             }
             console.log(id)
-            res.json("sucess")
+            res.status(204).json("sucess")
         },
 
-        updateRoom: function(req,res){
+        deleteRoom: function(req,res){
+            const id = req.params.id
 
-            const id = req.params.id;
-            const query = req.body
-            try{
-                const response = Room.findOneAndUpdate({_id:id},query).exec();
-            }catch{
-                res.sendStatus(404)
-            }
-            console.log(id)
-            console.log(response)
-            res.json("sucess")
+            Room.deleteOne({_id: id}, function(err){
+                if(!err)
+                    res.status(204).json({message: 'sucess'})
+                else
+                    res.status(500).end()
+            })
         }
-        
     
     }
 
