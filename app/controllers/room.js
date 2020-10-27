a6a227862a179db558e5fb7f32dc525b64a1c25f
Frederico T <frederico.thofehrn@acad.pucrs.br>
2020-10-26 22:03:38 -0300

    ajustando swagger c response 201 e erro de undefined para getbyid

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -1,6 +1,7 @@
 const { schema } = require('../../config/schemas/schemas')
 const querystring = require('querystring');
 const http = require('http');
+const { exception } = require('console');
 let classes = []
 let aulas = []
 
@@ -48,7 +49,7 @@ async function buildClasses(room_classes){
 async function fazAulas(sala_aulas){
 
     
-
+try{
     sala_aulas.forEach(element => {
 
         const url = `http://admin:admin@ec2-18-218-177-125.us-east-2.compute.amazonaws.com:3000/api/v1/classes/${element}`
@@ -73,6 +74,9 @@ async function fazAulas(sala_aulas){
         //aux_class.then(returnPromisse,returnPromisseError)
         //classes.push(element)
     });
+  } catch {
+    return null;
+  }
 
     return await Promise.all(aulas)
 }
@@ -84,11 +88,15 @@ module.exports = function(app){
 
         findById: function(req,res){
             const id = req.params.id
+          
             const response = Room.findById(id, async function(err, data){
                 //grupo 6
+                if (data == undefined) {
+                  res.send("not found");
+                  res.sendStatus(404);
+          }
                 const aux = await buildClasses(data._doc.turmas)
-                data._doc.turmas = aux
-
+                data._doc.turmas = aux    
                 //grupo 4
                 const aux_aulas = await fazAulas(data._doc.aulas)
                 data._doc.aulas = aux_aulas
@@ -96,7 +104,7 @@ module.exports = function(app){
                 else if(data) res.json(data);
                 else res.sendStatus(404);
             })
-        },
+          },
 
         getByAtributes: function(req,res){
             const atribute = req.params.atribute;
@@ -113,7 +121,10 @@ module.exports = function(app){
                 console.log(err);
 
                 if(err) res.status(500).end();
-                else res.json(room);
+                else{
+                   res.json(room);
+                   res.sendStatus(201);
+                }
             });
         },
 
